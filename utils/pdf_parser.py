6eddae3d0fd7df79df545e9aa021974b12d2ac65
suae0918 <online.hn209@hanoischool.net>
2025-11-01 22:58:17 +0700

    Create pdf_parser.py

---
+++ utils/pdf_parser.py
@@ -0,0 +1,10 @@
+from PyPDF2 import PdfReader
+
+def extract_text(uploaded_file):
+    reader = PdfReader(uploaded_file)
+    text = ""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "\n"
+    return text
